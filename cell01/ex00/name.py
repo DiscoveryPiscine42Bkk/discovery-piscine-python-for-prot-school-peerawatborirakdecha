fcba44c14df0aa76c55aff8c3cb68f380d150fd7
peerawatborirakdecha <fifa0q7r77@gmail.com>
2025-03-11 10:56:14 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,8 @@
+# name.py
+
+first_name = "พีรวัส"  # แทนที่ "Wil" ด้วยชื่อจริงของคุณ
+last_name = "บริรักษ์เดชา"   # แทนที่ "428" ด้วยนามสกุลของคุณ
+
+whole_name = last_name + first_name
+
+print(whole_name)
