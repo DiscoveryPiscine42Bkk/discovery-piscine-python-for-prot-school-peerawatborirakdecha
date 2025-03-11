74757356fa8dd679ba6fee1764e15c408d7ff579
peerawatborirakdecha <fifa0q7r77@gmail.com>
2025-03-11 11:40:19 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+# whatsyourname.py
+
+first_name = พีรวัส ("Hey, what's your first name?: ")
+last_name = บริรักษ์เดชา ("And your last name?: ")
+
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
