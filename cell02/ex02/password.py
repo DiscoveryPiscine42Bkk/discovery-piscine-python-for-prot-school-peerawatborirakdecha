08a5de301537db2ba6cae6b130d64d5e67c77762
peerawatborirakdecha <fifa0q7r77@gmail.com>
2025-03-11 11:59:08 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# password.py
+
+password = "Python is awesome"
+
+entered_password = input()
+
+if entered_password == password: 0-639311817
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
